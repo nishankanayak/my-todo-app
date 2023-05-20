92c456048b734a72fc6718244dd89a404e66717e
pythongit <nayak4nayak4@gmail.com>
2023-05-20 20:06:30 +0530

    implemented pages to the web todo

--- Home.py
+++ Home.py
@@ -2,6 +2,9 @@ import streamlit as st
 import webtodo_func
 
 todos=webtodo_func.get_todos()
+
+# st.set_page_config(layout="wide")
+
 def add_todo():
     todo=st.session_state["newtodo"]+"\n"
     todos.append(todo)
@@ -9,7 +12,10 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my Todo App")
-st.write("This app is to increase productivity")
+st.write("<i>This app is to increase productivity</i>",unsafe_allow_html=True)
+
+st.text_input(label="Enter a Todo",placeholder="Add a new Todo",
+              on_change=add_todo,key="newtodo")
 
 for index,todo in enumerate(todos):
     checkbox=st.checkbox(todo,key=todo)
@@ -19,5 +25,3 @@ for index,todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a Todo",placeholder="Add a new Todo",
-              on_change=add_todo,key="newtodo")
---
+++ Pages/About.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.subheader("My Todo App")
+st.write("Hello!")
